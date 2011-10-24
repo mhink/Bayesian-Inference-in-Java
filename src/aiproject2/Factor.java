874ed13c8e9cb5b4c57b74104a27a9677a85cfd2
Matt Hink <mhink1103@gmail.com>
2011-10-24 11:41:44 -0500

    Starting work on implementing marginalization of variables.

--- src/aiproject2/Factor.java
+++ src/aiproject2/Factor.java
@@ -11,19 +11,42 @@ import java.util.Arrays;
  * @author Hink
  */
 public class Factor {
-    RandomVariable dependent;
-    RandomVariable[] dependencies;
+    RandomVariable[] variables;
     Float[][] distribution;
     
-    public Factor(RandomVariable[] vars, Float[] distribution) {
-        
+    public Factor(RandomVariable[] vars, Float[][] distribution) {
+        this.variables = vars;
+        this.distribution = distribution;
     }
     
     public Factor pointwiseMultiplyBy(Factor other) {
         return null;
     }
     
-    public Factor sumOver(RandomVariable rv) {
+    public Factor marginalize(RandomVariable rv) {
+        int variablePosition = Arrays.asList(variables).indexOf(rv);
+        
+        Float[][][] marginalValues = new Float[rv.numStates()][0][0];
+        
+        for(int i = 0; i < distribution.length; i++) {
+            Arrays.asList(marginalValues[i%rv.numStates()]).add(distribution[i]);
+        }
+        
+        Float[][] marginalDistribution = marginalValues[0];
+        
+        for(int i = 1; i < marginalValues.length; i++) {
+            for(int j = 0; j < marginalDistribution.length; j++) {
+                for( int k = 0; k < marginalDistribution[0].length; k++) {
+                    
+                }
+            }
+        }
+        
+        
         return null;
+    }
+    
+    public Factor normalize() {
+        return this;
     }
 }
